4ffbea3008e291054c8dddfd0bb5984a90fb817d
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-02-07 19:22:20 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -122,10 +122,10 @@ async def pipcheck(pip):
 @register(outgoing=True, pattern="^\.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit(""
-                      f"Zinda hun {DEFAULTUSER} sir batao kiski maarni hai
+    await alive.edit(f"Zinda hun {DEFAULTUSER} sir batao kiski marni hai")
+                      
 
-                     "")
+                     
 
 
 @register(outgoing=True, pattern="^\.aliveu")
